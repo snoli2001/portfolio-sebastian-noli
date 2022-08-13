304ed41cc26b4bbe1159bdd2bff17a0c6bac9b64
snoli2001 <sebastian.noli@gmail.com>
2022-08-13 18:22:41 -0500

    experience section advanced

--- src/app/components/sections/experience/experience.component.ts
+++ src/app/components/sections/experience/experience.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.handleTabs();
   }
 
+  _class(name: string): HTMLCollectionOf<HTMLElement> {
+    return document.getElementsByClassName(
+      name
+    ) as HTMLCollectionOf<HTMLElement>;
+  }
+
+  handleTabs(): void {
+    let tabPanes = this._class('tab-header')[0].getElementsByTagName('div');
+
+    for (let i = 0; i < tabPanes.length; i++) {
+      tabPanes[i].addEventListener('click', () => {
+        this._class('tab-header')[0]
+          .getElementsByClassName('active')[0]
+          .classList.remove('active');
+
+        tabPanes[i].classList.add('active');
+
+        this._class('tab-indicator')[0].style.top = `calc(80px + ${i * 50}px)`;
+
+        this._class('tab-content')[0]
+          .getElementsByClassName('active')[0]
+          .classList.remove('active');
+
+
+        this._class('tab-content')[0]
+          .getElementsByTagName('div')
+          [i].classList.add('active');
+      });
+    }
+  }
 }
